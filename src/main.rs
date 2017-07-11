9b7adc599d9ba84ca92cf9615f69737d7161c40e
Igor Bykov <igorb.dev@websm.io>
2017-07-11 13:59:00 +0300

    save work

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,108 @@ use std::net::{
 
 use std::process::{
     Command,
-    Stdio
+    Stdio,
+    ExitStatus
 };
 
 use ipnetwork::{
     Ipv4Network,
     Ipv6Network
 };
+
+fn ping4(ip: Ipv4Addr) -> ExitStatus {
+
+    let mut child = Command::new("ping")
+        .arg("-c 1")
+        .arg("-i 0")
+        .arg("-W 10")
+        .arg("-o")
+        .arg(format!("{}", ip))
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("error");
+
+    let ecode = child.wait()
+        .expect("error");
+
+    ecode
+
+}
+
+fn ping6(ip: Ipv6Addr) -> ExitStatus {
+
+    let mut child = Command::new("ping6")
+        .arg("-c 1")
+        .arg("-i 0")
+        .arg("-X 10")
+        .arg("-o")
+        .arg(format!("{}", ip))
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("error");
+
+    let ecode = child.wait()
+        .expect("error");
+
+    ecode
+
+}
+
+fn get_free4(ip: &str) {
+
+    let network: Ipv4Network = ip.parse().unwrap();
+    let network_addr = network.network();
+    let broadcast = network.broadcast();
+
+    for addr in network.iter() {
+
+        if addr == broadcast || addr == network_addr {
+            continue; 
+        }
+
+        let ecode = ping4(addr);
+
+        if ecode.success() { continue; }
+
+        match ecode.code().unwrap() {
+
+            2 => println!("free: {:?}", addr),
+            _ => break
+
+        }
+
+        break;
+
+    }
+
+}
+
+fn get_free6(ip: &str) {
+
+    let network: Ipv6Network = ip.parse().unwrap();
+
+    for addr in network.iter() {
+
+        let ecode = ping6(addr);
+
+        if ecode.success() { continue; }
+
+        match ecode.code().unwrap() {
+
+            2 => println!("free: {:?}", addr),
+            _ => break
+
+        }
+
+        break;
+
+    }
+
+}
 
 fn main() {
 
@@ -31,6 +126,17 @@ fn main() {
 
     println!("{:?}", v4);
     println!("{:?}", v6);
+
+    match (v6) {
+        Some(ip) => get_free6(ip),
+        None => ()
+    }
+
+    match (v4) {
+        Some(ip) => get_free4(ip),
+        None => ()
+    }
+
 
     // let network: Ipv4Network = "192.168.0.1/24".parse().unwrap();
     // let network_addr = network.network();
